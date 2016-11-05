7925b573226a06c65b3dd0c192f2555e1be47227
perfectworks <perfectworks@gmail.com>
2016-11-05 13:43:23 +0800

    init commit

---
+++ pages/index/index.js
@@ -0,0 +1,31 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+    data: {
+        motto: 'Hello World',
+        userInfo: {}
+    },
+    //事件处理函数
+    bindViewTap: function() {
+        wx.navigateTo({
+            url: '../logs/logs'
+        })
+    },
+    onLoad: function () {
+        //调用应用实例的方法获取全局数据
+        app.getUserInfo(userInfo => {
+            //更新数据
+            this.setData({
+                userInfo: userInfo
+            });
+        });
+
+        let count = 0;
+        setInterval(() => {
+            this.setData({
+                motto: count++
+            });
+        }, 1000);
+    }
+})
